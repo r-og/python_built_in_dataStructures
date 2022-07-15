5ee45b23bcf475654f24fa11b30e1ff553c5d722
ro-g <rseverson13@protonmail.com>
2022-07-15 13:19:34 -0500

    added more documentation, but program is done to MY expectation

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 Author: Roger Severson
 Date Started: July 15, 2022
-
+______________________________________________________________________
 Task: list_game.py
 
 1. Create list with a bunch of random words
 2. Allow user to enter a letter and grab all words starting with that word from list
 3. Add these words to a new list and print them to the user
+----------------------------------------------------------------------
 
+
+______________________________________________________________________
 Task: dictionary_game.py
 
 1. This game will use a dictionary as a way to add items to a grocery "list" dictionary, a list would be better but I want to use
     a dictionary.
 2. The user will select a key, key should not be in the dictionary already
+----------------------------------------------------------------------
--- dictionary_game.py
+++ dictionary_game.py
@@ -1,3 +1,7 @@
+import time
+
+from cv2 import goodFeaturesToTrack # for visual
+
 grocery_items = {1: 'test'} # empty dictionary to store items, first item added for testing
 
 # allows the user to set a key for the item entered into dictionary
@@ -22,12 +26,37 @@ def enter_item():
     while True:
         try:
             get_item = input('Enter a grocery item to add to the list: ')
-            for item in grocery_items:
-                if get_item in grocery_items:
-                    print('Item already in dictionary.. try again\n')
-                    continue
-                else:
-                     return get_item
+            if get_item in grocery_items.values(): # .values() used to check if value is in dictionary
+                print('Item already in dictionary.. try again\n')
+                continue
+            else:
+                return get_item
             
         except:
             print('Something wrong occurred... try again\n')
+            continue
+
+
+# full program to enter a grocery item into a dictionary
+def enter_grocery_item():
+    while True:
+        try:
+            add_item = input('Would you like to enter an item? y/n ')
+            if add_item == 'y' or add_item == 'Y':
+                key = enter_key()
+                item = enter_item()
+
+                grocery_items[key] = item
+                time.sleep(2)
+                print('item added')
+                continue
+            else:
+                print('\nHere is your grocery list:')
+                return grocery_items
+
+        except:
+            print('Something went wrong! try again\n')
+            continue
+
+
+print(enter_grocery_item())
